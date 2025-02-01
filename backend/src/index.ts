ddb038c63915340c3dcc8b6617da03ab58f651cc
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-02-02 03:16:19 +0530

    FE init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,21 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
+    
     userCount++;
     console.log("user connected #" + userCount);
 
     socket.on("message", (message) => {
         console.log("Message recieved " + message.toString());
-        setTimeout(() => {
-            socket.send("recieved.")
-        }, 1000)
+        for(let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(message.toString() + " recieved.");
+        }
     })
 })
