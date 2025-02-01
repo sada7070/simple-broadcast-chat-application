962a3d6c56e0dd689a848d2cbbe55588b02eaa77
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-02-02 04:53:33 +0530

    added room logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,20 +2,41 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+interface User {
+    socket: WebSocket;
+    room: string;   
+}
+
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    
-    userCount++;
-    console.log("user connected #" + userCount);
-
     socket.on("message", (message) => {
-        console.log("Message recieved " + message.toString());
-        for(let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            s.send(message.toString() + " recieved.");
+        // there is no JSON obj in websocket, so the incoming values will be in string. So converting string to JSON
+        const parsedMessage = JSON.parse(message as unknown as string);
+
+        if(parsedMessage.type == "join") {
+            // if the user wants to join an existing room then,
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
         }
+
+        if(parsedMessage.type == "chat") {
+            // if the user wants to send a message, then find that user's room.
+            const currentUserRoom = allSockets.find((x) => x.socket == socket)?.room
+
+            // to send message to all the sockets of that room
+            for(let i = 0; i < allSockets.length; i++) {
+                if(allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
+        }
+    })
+
+    // to clean up disocnnected sockets.
+    socket.on("disconnect", () => {
+        allSockets = allSockets.filter(x => x.socket != socket);
     })
 })
